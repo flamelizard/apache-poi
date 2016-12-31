e454380921665b4de73010b5dc91638d1e0f9281
Tom Sedlak <tom.stranger2@gmail.com>
2016-12-31 17:14:21 +0100

    How to - Apache POI - read/write Excel

---
+++ main/java/RatingUpdate/SourceDataException.java
@@ -0,0 +1,12 @@
+package RatingUpdate;
+
+import java.io.IOException;
+
+/**
+ * Created by Tom on 12/30/2016.
+ */
+public class SourceDataException extends IOException {
+    public SourceDataException(String msg) {
+        super(msg);
+    }
+}
